540691fa4cf98198e220b008b1deb3f62e54a1cb
EugeneS30 <shragovich@gmail.com>
2015-07-13 23:02:48 +0800

    Added logs and handling for non-completed test steps

--- src/main/java/com/github/eugene/containers/Step.java
+++ src/main/java/com/github/eugene/containers/Step.java
@@ -1,15 +1,33 @@
 package com.github.eugene.containers;
 
+import lombok.extern.slf4j.Slf4j;
+
 import org.json.simple.JSONObject;
 
+@Slf4j
 public class Step {
 
 	private String name;
 	private String result;
+	private long duration;
 
 	public Step(String name, JSONObject result) {
+	    log.debug("Step constructor start...");
+	    
 		this.name = name;
 		this.result = (String) result.get("status");
+		if ("skipped".equals(this.result)) {
+		    log.warn("SKIPPED test. Setting duration to -1");
+		    this.duration = -1;
+		}
+		else if ("undefined".equals(this.result)) {
+		    log.warn("UNDEFINED test.  Setting duration to -2");
+		    this.duration = -2;
+		}
+		else {
+		    this.duration = (Long) result.get("duration");
+		}
+
 	}
 
 	public boolean isFailed() {
