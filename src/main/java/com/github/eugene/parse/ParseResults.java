1ec4d9bf5f8cab48bb4114a7d649a9b4aa4bb7a3
EugeneS30 <shragovich@gmail.com>
2015-07-13 23:03:10 +0800

    Added logs. Changed order of execution

--- src/main/java/com/github/eugene/parse/ParseResults.java
+++ src/main/java/com/github/eugene/parse/ParseResults.java
@@ -7,6 +7,8 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import lombok.extern.slf4j.Slf4j;
+
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
@@ -15,61 +17,66 @@ import org.json.simple.parser.ParseException;
 import com.github.eugene.containers.FeatureFileElement;
 import com.github.eugene.containers.Scenario;
 
+@Slf4j
 public class ParseResults {
 
-	private static final String filePath = "C:\\Users\\Polar\\Google Drive\\work\\376\\cucumber.json";
-	private static final String myDirectoryPath = "C:\\Users\\Polar\\Google Drive\\work";
+    //private static final String filePath = "C:\\Users\\Polar\\Google Drive\\work\\376\\cucumber.json";
+    private static final String myDirectoryPath = "C:\\Users\\Polar\\Google Drive\\work";
 
-	public static void main(String[] args) {
-		File dir = new File(myDirectoryPath);
-		File[] directoryListing = dir.listFiles();
-		if (directoryListing != null) {
-			for (File child : directoryListing) {
-				System.out.println(child);
-			}
-		} else {
-			// Handle the case where dir is not really a directory.
-			// Checking dir.isDirectory() above would not be sufficient
-			// to avoid race conditions with another process that deletes
-			// directories.
-		}
-		try {
-			FileReader reader = new FileReader(filePath);
-			JSONParser jsonParser = new JSONParser();
+    public static void main(String[] args) throws Exception {
+        
+        File dir = new File(myDirectoryPath);
+        File[] directoryListing = dir.listFiles();
+        
+        List<List<FeatureFileElement>> buildResults = new ArrayList<List<FeatureFileElement>>();
 
-			JSONArray jsonFileData = (JSONArray) jsonParser.parse(reader);
+        if (directoryListing != null) {
 
-			List<JSONObject> allJSONFileElementsList = new ArrayList<JSONObject>();
-			allJSONFileElementsList.addAll(jsonFileData);
+            for (File buildPath : directoryListing) {
+                
+                try {
+                    FileReader reader = new FileReader(String.join("\\", buildPath.toString(), "cucumber.json"));
+                    JSONParser jsonParser = new JSONParser();
 
-			List<FeatureFileElement> featureFileObjectsList = new ArrayList<FeatureFileElement>();
+                    JSONArray jsonFileData = (JSONArray) jsonParser.parse(reader);
+                    List<JSONObject> allJSONFileElementsList = new ArrayList<JSONObject>();
+                    allJSONFileElementsList.addAll(jsonFileData);
 
-			for (JSONObject featureFileElement : allJSONFileElementsList) {
-				String name = (String) featureFileElement.get("name");
-				String uri = (String) featureFileElement.get("uri");
-				JSONArray elements = (JSONArray) featureFileElement
-						.get("elements");
+                    List<FeatureFileElement> featureFileObjectsList = new ArrayList<FeatureFileElement>();
 
-				featureFileObjectsList.add(new FeatureFileElement(name, uri,
-						elements));
-			}
+                    int iter = 0;
+                    for (JSONObject featureFileElement : allJSONFileElementsList) {
+                        log.info("Processing new featureFileElement: " + iter);
+                        iter ++;
+                        String name = (String) featureFileElement.get("name");
+                        String uri = (String) featureFileElement.get("uri");
+                        JSONArray elements = (JSONArray) featureFileElement.get("elements");
 
-			for (FeatureFileElement featureFileElement : featureFileObjectsList) {
-				featureFileElement.getScenariosResults();
-				System.out.println("=-=-=-=-=-=-=-=-=-");
-			}
+                        featureFileObjectsList.add(new FeatureFileElement(name, uri, elements));
+                        
+                        buildResults.add(featureFileObjectsList);
+                    }
+                }
 
-		}
+                catch (FileNotFoundException ex) {
+                    ex.printStackTrace();
+                } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                } catch (ParseException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
 
-		catch (FileNotFoundException ex) {
-			ex.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (ParseException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
 
-	}
+            }
+        } else {
+            // Handle the case where dir is not really a directory.
+            // Checking dir.isDirectory() above would not be sufficient
+            // to avoid race conditions with another process that deletes
+            // directories.
+        }
+
+        
+    }
 }
