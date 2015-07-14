73061b0dd839030a71fcaaab86cb11b2250e186a
ez002622 <ez002622@MSF0795991157F.int.det.wa.edu.au>
2015-07-14 17:03:23 +0800

    Created test class

---
+++ src/test/java/com/github/eugene/testClass.java
@@ -0,0 +1,19 @@
+package com.github.eugene;
+
+import java.io.FileOutputStream;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import lombok.extern.slf4j.Slf4j;
+
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.ss.util.*;
+import org.apache.poi.ss.usermodel.charts.*;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+@Slf4j
+public class testClass {
+    
+    Path path = Paths.get("foo", "bar", "baz.txt");
+
+	}
