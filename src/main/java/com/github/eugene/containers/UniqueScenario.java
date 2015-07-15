69d6293e8838da452b4647229d059bbf466064cc
ez002622 <ez002622@MSF0795991157F.int.det.wa.edu.au>
2015-07-15 11:42:40 +0800

    Created UniqueScenario class

---
+++ src/main/java/com/github/eugene/containers/UniqueScenario.java
@@ -0,0 +1,14 @@
+package com.github.eugene.containers;
+
+import java.util.List;
+
+public class UniqueScenario {
+    private String scenarioName;
+    private String uri;
+    List<Scenario> uniqueScenariosList;
+    
+    public UniqueScenario() {
+        
+    }
+
+}
