b6b9f8b06e5ae9b5f3a7485014f76a6c8bfa9215
ez002622 <ez002622@MSF0795991157F.int.det.wa.edu.au>
2015-08-05 15:35:46 +0800

    Results pairs implementation

--- src/main/java/com/github/eugene/parse/ParseResults.java
+++ src/main/java/com/github/eugene/parse/ParseResults.java
@@ -5,11 +5,8 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
@@ -17,8 +14,6 @@ import java.util.TreeMap;
 import lombok.extern.slf4j.Slf4j;
 
 import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.CellStyle;
-import org.apache.poi.ss.usermodel.IndexedColors;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
@@ -28,9 +23,7 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
-import com.github.eugene.config.AppConfig;
 import com.github.eugene.containers.FeatureFileElement;
-import com.github.eugene.containers.Scenario;
 
 @Slf4j
 public class ParseResults {
@@ -73,12 +66,13 @@ public class ParseResults {
                     JSONArray elements = (JSONArray) featureFileElement.get("elements");
 
                     featureFileObjectsList.add(new FeatureFileElement(name, uri, elements));
-
+                    
                     // buildResults.add(featureFileObjectsList);
 
                 }
 
                 buildsResults.put(buildNumber, featureFileObjectsList);
+                
             }
 
             catch (FileNotFoundException e) {
@@ -93,50 +87,75 @@ public class ParseResults {
 
         }
         
-        
+       
         
         Workbook wb = new XSSFWorkbook();
-        
-//        CellStyle style = wb.createCellStyle(); 
-//        style = wb.createCellStyle();
-//        style.setFillForegroundColor(IndexedColors.ORANGE.getIndex());
-//        style.setFillPattern(CellStyle.SOLID_FOREGROUND);
-        
+
+        // CellStyle style = wb.createCellStyle();
+        // style = wb.createCellStyle();
+        // style.setFillForegroundColor(IndexedColors.ORANGE.getIndex());
+        // style.setFillPattern(CellStyle.SOLID_FOREGROUND);
+
         Sheet sheet = wb.createSheet("Test Results");
         Row row = sheet.createRow(0);
-        
+
         Cell cellFilename = row.createCell(0);
         Cell cellScenario = row.createCell(1);
-        
+
         cellFilename.setCellValue("Filename");
         cellScenario.setCellValue("Scenario");
-                
-        /*
-         * Iterate over FeatureFileElement objects
-         */
+
+        
         int startCol = 2;
+        int startRow = 1;
+        /*
+         * Iterate over builds
+         */
         for (Map.Entry<Integer, List<FeatureFileElement>> entry : buildsResults.entrySet()) {
-            List<FeatureFileElement> featureElements = entry.getValue();
+            System.out.println("=-=-=-=-=-=-=-=-=-=-=");
+            
+            List<FeatureFileElement> featureFileElements = entry.getValue();
 
             Integer key = entry.getKey();
             List<FeatureFileElement> value = entry.getValue();
-            
+
             Cell cell = row.createCell(startCol);
             cell.setCellValue(key);
-                        
-            for (FeatureFileElement featureElement : featureElements) {
-                Row row1 = sheet.createRow(1);
-                for (Scenario scenario : featureElement.getScenarios()) {
-                    log.info(scenario.toString());
-                }
-            }
             
+            Row rowTest = sheet.createRow(startRow);
+            Cell cellBuildResult = rowTest.createCell(startCol);
+            //cellBuildResult.setCellValue();
             
-            startCol++;            
-        }
-        
-        FileOutputStream fileOut = new FileOutputStream("workbook.xlsx");
+            /*
+             * Iterate over FeatureFileElements (currently 58)
+             */
+            int j = 0;
+            for (FeatureFileElement ffe : featureFileElements) {
+                j++;
+                Map<String, Boolean> resultPairs = ffe.generateScenarioResultPairs();
                 
+                for (Map.Entry<String, Boolean> resultPair : resultPairs.entrySet()) {
+                    Cell c = rowTest.createCell(startCol);
+                }
+                
+            }
+            System.out.println(j);
+
+
+//            for (FeatureFileElement featureElement : featureElements) {
+//                Row rowTest = sheet.createRow(startRow);
+//                for (Scenario scenario : featureElement.getScenarios()) {
+//                    Cell cellBuildResult = rowTest.createCell(startCol);
+//                    cellBuildResult.setCellValue(scenario.getScenarioName() + scenario.getScenarioType());
+//                    startRow++;
+//                }
+//            }
+
+            startCol++ ;
+        }
+
+        FileOutputStream fileOut = new FileOutputStream("workbook.xlsx");
+
         wb.write(fileOut);
         fileOut.close();
 
