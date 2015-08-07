04c1b0e467869d14bfbf0e8d1b56a44788dce313
ez002622 <ez002622@MSF0795991157F.int.det.wa.edu.au>
2015-08-07 16:17:03 +0800

    excel construction and formatting work

--- src/main/java/com/github/eugene/parse/ParseResults.java
+++ src/main/java/com/github/eugene/parse/ParseResults.java
@@ -13,15 +13,22 @@ import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
+import java.util.SortedSet;
 import java.util.TreeMap;
+import java.util.TreeSet;
 
 import lombok.extern.slf4j.Slf4j;
 
+import org.apache.poi.hssf.record.cf.PatternFormatting;
 import org.apache.poi.ss.formula.functions.Column;
 import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.Font;
+import org.apache.poi.ss.usermodel.IndexedColors;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.util.SystemOutLogger;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
@@ -96,36 +103,37 @@ public class ParseResults {
         for (Entry<Integer, List<FeatureFileElement>> build: buildsResults.entrySet()) {
             for (FeatureFileElement ffe : build.getValue()){
                 for (Scenario scenario : ffe.getScenarios()) {
-                    allBuildResults.add(new UniqueScenario(build.getKey(), scenario, scenario.isFailed()));
+                    allBuildResults.add(new UniqueScenario(build.getKey(), scenario, scenario.getRunResult()));
                 }
             }
         }
         
         // Generates a list of unique scenario names from all the builds.
-        Set<String> uniqueScenarioNamesSet = new HashSet<String>();
+        //Set<String> uniqueScenarioNamesSet = new HashSet<String>();
+        SortedSet<String> uniqueScenarioNamesSet = new TreeSet<String>();
         
         for (UniqueScenario scenario : allBuildResults) {
             uniqueScenarioNamesSet.add(scenario.generateUriScenarioPair());
         }
-         
+        
+                
         // A list of all build numbers
         List<Integer> buildsList = new ArrayList<Integer>();
         for (Integer buildNum : buildsResults.keySet()) {
             buildsList.add(buildNum);
         }
         
-        //Map<String, List<Map<Integer, Boolean>>> finalResultsMap = new HashMap<String, List<Map<Integer, Boolean>>>();
-        
+
         Workbook wb = new XSSFWorkbook();
 
-        // CellStyle style = wb.createCellStyle();
-        // style = wb.createCellStyle();
-        // style.setFillForegroundColor(IndexedColors.ORANGE.getIndex());
-        // style.setFillPattern(CellStyle.SOLID_FOREGROUND);
-
-        Sheet sheet = wb.createSheet("Test Results");
-        Row topRow = sheet.createRow(0);
+        /**
+         * Initial formatting
+         */
+        Sheet resultsSheet = wb.createSheet("Test Results");
+        Sheet runStatsSheet = wb.createSheet("Run Statistics");
         
+        Row topRow = resultsSheet.createRow(0);
+                
         Cell cellFilename = topRow.createCell(0);
         Cell cellScenario = topRow.createCell(1);
         
@@ -152,11 +160,11 @@ public class ParseResults {
         int currentRowNumber = 1;
         for (String uniqueScenario : uniqueScenarioNamesSet) {
             
-            Row currentRow = sheet.createRow(currentRowNumber);
+            Row currentRow = resultsSheet.createRow(currentRowNumber);
             Cell currentFilenameCell = currentRow.createCell(0);
             Cell currentScenarioCell = currentRow.createCell(1);
             
-            scenarioNameToRowMapper.put(uniqueScenario.split(",")[1], currentRowNumber);
+            scenarioNameToRowMapper.put(uniqueScenario, currentRowNumber);
             
             //TODO use constants here instead of 0 and 1
             currentFilenameCell.setCellValue(uniqueScenario.split(",")[0]); //set the feature file path
@@ -164,70 +172,79 @@ public class ParseResults {
             currentRowNumber++;
         }
         
+      
+        CellStyle fillColorGreen = wb.createCellStyle();
+        CellStyle fillColorRed = wb.createCellStyle();
+        CellStyle fillColorYellow = wb.createCellStyle();
+        CellStyle foreGroundFill = wb.createCellStyle();
+        
+        fillColorGreen.setFillForegroundColor(IndexedColors.LIGHT_GREEN.getIndex());
+        fillColorRed.setFillForegroundColor(IndexedColors.ROSE.getIndex());
+        fillColorYellow.setFillForegroundColor(IndexedColors.YELLOW.getIndex());
+        
+        fillColorGreen.setFillPattern(PatternFormatting.SOLID_FOREGROUND);
+        fillColorRed.setFillPattern(CellStyle.SOLID_FOREGROUND);
+        fillColorYellow.setFillPattern(CellStyle.BIG_SPOTS);
         
         
-        //int currentCol = -1;
-        //int currentRow = -1;
         for (UniqueScenario entry : allBuildResults) {
-            System.out.println("=-=-=-=-=-=-=-=-=-=-=");
             
-            //List<FeatureFileElement> featureFileElements = entry.getValue();
-
             int buildNum = entry.getBuildNum();
             Scenario scenario = entry.getScenario();
-            boolean scenarioFailStatus = entry.isFailed();
+            String scenarioRunResult = entry.getRunResult();
+            String scenarioName = scenario.getScenarioName();
             
-//            if (finalResultsMap.get(scenario.getUri()) != null) {
-//                finalResultsMap.put(buildNum, scenario.isFailed())
-//            }
+            log.debug("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
+            log.debug("Processing UniqueScenario: #" + scenarioName + "#");
+         
             
             int currentColNum = buildToColumnMapper.get(buildNum);
-            int currentRowNum = scenarioNameToRowMapper.get(scenario.getScenarioName());
+            int currentRowNum = scenarioNameToRowMapper.get(scenario.generateUriScenarioPair());
             
-            log.info("Current cell and row:");
-            System.out.println(currentColNum + "," + currentRowNum);
+            Row currentRow = resultsSheet.getRow(currentRowNum);
             
-            Row currentRow = sheet.createRow(currentRowNum);
+            Cell currentCell = currentRow.createCell(currentColNum);
+            currentCell.setCellValue(scenarioRunResult);
             
-            Cell cell = currentRow.createCell(currentColNum);
-            cell.setCellValue(scenarioFailStatus);
+            if ("pass".equals(scenarioRunResult)) {
+                currentCell.setCellStyle(fillColorRed);
+            }
             
+            else if ("fail".equals(scenarioRunResult)) {
+                currentCell.setCellStyle(fillColorGreen);
+            }
             
-            //cellBuildResult.setCellValue();
+            else if ("pending".equalsIgnoreCase(scenarioRunResult)) {
+                currentCell.setCellStyle(fillColorYellow);
+            }
             
-            /*
-             * Iterate over FeatureFileElements (currently 58)
-             */
-//            int j = 0;
-//            for (FeatureFileElement ffe : featureFileElements) {
-//                j++;
-//                Map<String, Boolean> resultPairs = ffe.generateScenarioResultPairs();
-//                
-//                for (Map.Entry<String, Boolean> resultPair : resultPairs.entrySet()) {
-//                    Cell c = rowTest.createCell(startCol);
-//                }
-//                
-//            }
-//            System.out.println(j);
-
-
-//            for (FeatureFileElement featureElement : featureElements) {
-//                Row rowTest = sheet.createRow(startRow);
-//                for (Scenario scenario : featureElement.getScenarios()) {
-//                    Cell cellBuildResult = rowTest.createCell(startCol);
-//                    cellBuildResult.setCellValue(scenario.getScenarioName() + scenario.getScenarioType());
-//                    startRow++;
-//                }
-//            }
-
-            startCol++ ;
+            else {
+                log.error(scenarioRunResult.toString());
+                throw new UnsupportedOperationException(scenarioRunResult.toString());
+            }
+            
         }
-
+        
+        /**
+         * Final formatting
+         */
+        CellStyle style = wb.createCellStyle();
+        Font font = wb.createFont();
+        
+        font.setBoldweight(Font.BOLDWEIGHT_BOLD);
+        style.setFont(font);
+        
+        for(int i = 0; i < topRow.getLastCellNum(); i++){//For each cell in the row 
+            resultsSheet.autoSizeColumn(i);
+            topRow.getCell(i).setCellStyle(style);//Set the style
+        }
+        
+        resultsSheet.setColumnWidth(1, 20000);
+        
         FileOutputStream fileOut = new FileOutputStream("workbook.xlsx");
 
         wb.write(fileOut);
         fileOut.close();
-
     }
 }
 
