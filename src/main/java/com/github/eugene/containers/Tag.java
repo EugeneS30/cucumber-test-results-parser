7644f24ccba97adf04a870329b2715074703b070
ez002622 <ez002622@MSF0795991157F.int.det.wa.edu.au>
2015-08-17 15:17:43 +0800

    added all tags

---
+++ src/main/java/com/github/eugene/containers/Tag.java
@@ -0,0 +1,6 @@
+package com.github.eugene.containers;
+
+public enum Tag {
+	Classes, Communities, Core, Discussions, Feedback, KnownIssue, LearningContent, Manual, Marksbook, Notices, RequiresHover, Submissions, smoke
+
+}
