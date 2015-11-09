cf38ce5dac5f8f7460ecf2d8ea40c753823d7b1a
eugene.shragovich <shragovich@gmail.com>
2015-11-09 12:31:35 +0800

    fixed passed scenarios count. added failed scenarios count

--- src/test/java/com/github/eugene/parse/ParseResultsTest.java
+++ src/test/java/com/github/eugene/parse/ParseResultsTest.java
@@ -18,7 +18,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import org.apache.log4j.Logger;
-//import org.apache.poi.common.usermodel.Hyperlink;
+// import org.apache.poi.common.usermodel.Hyperlink;
 import org.apache.poi.hssf.record.cf.PatternFormatting;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.CellStyle;
@@ -40,6 +40,7 @@ import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 import org.junit.Test;
 
+import com.github.eugene.config.ConfigurationClass;
 import com.github.eugene.containers.FeatureFileElement;
 import com.github.eugene.containers.Scenario;
 import com.github.eugene.containers.UniqueScenario;
@@ -48,8 +49,29 @@ public class ParseResultsTest {
 
     final static Logger log = Logger.getLogger(ParseResultsTest.class);
 
-    private static final String buildsDirPath = "C:\\.jenkins\\jobs\\Connect Automated Functional Tests - Firefox 32\\builds";
-    private static final String jsonRelativePath = "\\htmlreports\\Functional_Test_Report\\cucumber.json";
+    private final String buildsDirPath = ConfigurationClass.buildsDirPath;
+    private final String jsonRelativePath = ConfigurationClass.jsonRelativePath;
+
+    private static int getExcelColumnNumber(String column) {
+        int result = 0;
+        for (int i = 0; i < column.length(); i++ ) {
+            result *= 26;
+            result += column.charAt(i) - 'A' + 1;
+        }
+        return result;
+    }
+
+    public static String getExcelColumnName(int number) {
+        final StringBuilder sb = new StringBuilder();
+
+        int num = number - 1;
+        while (num >= 0) {
+            int numChar = (num % 26) + 65;
+            sb.append((char) numChar);
+            num = (num / 26) - 1;
+        }
+        return sb.reverse().toString();
+    }
 
     @Test
     public void testFunction() throws IOException {
@@ -137,7 +159,7 @@ public class ParseResultsTest {
          */
 
         Workbook workbook = new XSSFWorkbook();
-        
+
         /**
          * Pre formatting (first row and column)
          */
@@ -146,23 +168,23 @@ public class ParseResultsTest {
         Font fontArial = workbook.createFont();
         fontArial.setFontHeightInPoints((short) 10);
         fontArial.setFontName("Arial");
-        //fontArial.setBoldweight(Font.BOLDWEIGHT_NORMAL);
-        
+        // fontArial.setBoldweight(Font.BOLDWEIGHT_NORMAL);
+
         Font fontArialBold = workbook.createFont();
         fontArialBold.setFontHeightInPoints((short) 10);
         fontArialBold.setFontName("Arial");
         fontArialBold.setBoldweight(Font.BOLDWEIGHT_BOLD);
-        
+
         // Styles
         CellStyle resultCellsStyle = workbook.createCellStyle();
         resultCellsStyle.setFont(fontArial);
-        
+
         CellStyle topRowStyle = workbook.createCellStyle();
         topRowStyle.setFont(fontArialBold);
 
         Sheet resultsSheet = workbook.createSheet("Test Results");
         Sheet runStatsSheet = workbook.createSheet("Run Statistics");
-        
+
         resultsSheet.createFreezePane(0, 1);
 
         Row topRow = resultsSheet.createRow(0);
@@ -202,9 +224,8 @@ public class ParseResultsTest {
             scenarioNameToRowMapper.put(uniqueScenario, currentRowNumber);
 
             // TODO use constants here instead of 0 and 1
-            currentFilenameCell.setCellValue(uniqueScenario.split(",")[0]); // set the feature file
+            currentFilenameCell.setCellValue(uniqueScenario.split(",")[0]); // set feature file path
             currentFilenameCell.setCellStyle(resultCellsStyle);
-                                                                            // path
             currentScenarioCell.setCellValue(uniqueScenario.split(",")[1]); // set the scenario name
             currentScenarioCell.setCellStyle(resultCellsStyle);
             currentRowNumber++ ;
@@ -218,23 +239,23 @@ public class ParseResultsTest {
         fillColorGreen.setFillForegroundColor(IndexedColors.LIGHT_GREEN.getIndex());
         fillColorGreen.setFillPattern(PatternFormatting.SOLID_FOREGROUND);
         fillColorGreen.setFont(fontArial);
-        
+
         fillColorRed.setFillForegroundColor(IndexedColors.ROSE.getIndex());
         fillColorRed.setFillPattern(CellStyle.SOLID_FOREGROUND);
         fillColorRed.setFont(fontArial);
- 
+
         fillColorYellow.setFillForegroundColor(IndexedColors.LEMON_CHIFFON.getIndex());
         fillColorYellow.setFillPattern(CellStyle.SOLID_FOREGROUND);
         fillColorGreen.setFont(fontArial);
 
         CellStyle hlink_style = workbook.createCellStyle();
         CreationHelper createHelper = workbook.getCreationHelper();
-        
+
         Font hlink_font = workbook.createFont();
         hlink_font.setUnderline(Font.U_SINGLE);
         hlink_font.setColor(IndexedColors.BLUE.getIndex());
         hlink_style.setFont(hlink_font);
-        
+
         for (UniqueScenario entry : allBuildResults) {
 
             int buildNum = entry.getBuildNum();
@@ -259,43 +280,43 @@ public class ParseResultsTest {
             Drawing drawing = resultsSheet.createDrawingPatriarch();
             ClientAnchor anchor = factory.createClientAnchor();
             RichTextString str = factory.createRichTextString(tags.toString());
-            
+
             if ("pass".equalsIgnoreCase(scenarioRunResult)) {
                 currentCell.setCellStyle(fillColorGreen);
             }
 
-                        
             else if ("fail".equalsIgnoreCase(scenarioRunResult)) {
                 String screenShotName = "";
-                
+
                 Pattern pattern = Pattern.compile("screenshot-\\d*");
                 try {
                     Matcher matcher = pattern.matcher(scenario.getScreenShotPath());
-                    
+
                     if (matcher.find()) {
                         screenShotName = matcher.group(0);
                     }
+                } catch (NullPointerException e) {
+                    log.error("Couldn't get screenshotpath for scenario: \"" + entry.getScenarioName() + "\" in build "
+                            + entry.getBuildNum());
                 }
-                catch (NullPointerException e ) {
-                    log.error("Couldn't get screenshotpath for scenario: \"" + entry.getScenarioName() + "\" in build " +  entry.getBuildNum());
-                }
-                
-                String URL = "http://10.25.67.130:8080/job/Connect%20Automated%20Functional%20Tests%20-%20Firefox%2032/" + buildNum + "/Functional_Test_Report/screenshots/" + screenShotName + ".png";
+
+                String URL = "http://10.25.67.130:8080/job/Connect%20Automated%20Functional%20Tests%20-%20Firefox%2032/" + buildNum
+                        + "/Functional_Test_Report/screenshots/" + screenShotName + ".png";
 
                 currentCell.setCellStyle(fillColorRed); // Set style
-                
+
                 // Check if known issue
                 if (tags.contains("KnownIssue")) {
                     Comment comment = drawing.createCellComment(anchor);
                     comment.setString(str);
                     currentCell.setCellComment(comment); // Assign the comment to the cell
                 }
-                
+
                 Hyperlink link = createHelper.createHyperlink(Hyperlink.LINK_FILE);
                 link.setAddress(URL);
-                
+
                 currentCell.setHyperlink(link);
-                //currentCell.setCellStyle(hlink_style);
+                // currentCell.setCellStyle(hlink_style);
             }
 
             else if ("pending".equalsIgnoreCase(scenarioRunResult)) {
@@ -306,27 +327,36 @@ public class ParseResultsTest {
                 log.error(scenarioRunResult.toString());
                 throw new UnsupportedOperationException(scenarioRunResult.toString());
             }
-            
-            
+
         }
 
         /**
          * Post processing
          */
-        
-        int lastRowNum = resultsSheet.getLastRowNum();
-        int statsRowNum = lastRowNum + 2;
-        Row statsRow = resultsSheet.createRow(lastRowNum);
-        
 
-        for (int i = 2; i < topRow.getLastCellNum(); i++ ) {// For each cell in the row
-            Cell cell = statsRow.createCell(i);
-            cell.setCellFormula("COUNTIF(C2:C341, \"Pass\")");
+        int lastRowNum = resultsSheet.getLastRowNum();
+        Row passStatsRow = resultsSheet.createRow(lastRowNum);
+        Row failStatsRow = resultsSheet.createRow(lastRowNum + 1);
+
+        // Count and print the number of passed scenarios
+        for (int i = 2; i < topRow.getLastCellNum(); i++ ) { // For each cell in the row
+            Cell cell = passStatsRow.createCell(i);
+            String colName = getExcelColumnName(i + 1); // "Translate" the column number to column
+                                                        // name. Example: C = 2
+            String formula = "COUNTIF(" + colName + "2:" + colName + "341, \"Pass\")";
+            cell.setCellFormula(formula);
         }
-        
-        
-        Cell statsCell = statsRow.createCell(1);
-        
+
+        // Count and print the number of failed scenarios
+        for (int i = 2; i < topRow.getLastCellNum(); i++ ) { // For each cell in the row
+            Cell cell = failStatsRow.createCell(i);
+            String colName = getExcelColumnName(i + 1); // "Translate" the column number to column
+                                                        // name. Example: C = 2
+            String formula = "COUNTIF(" + colName + "2:" + colName + "341, \"Fail\")";
+            cell.setCellFormula(formula);
+        }
+
+        // Cell statsCell = statsRow.createCell(1);
 
         for (int i = 0; i < topRow.getLastCellNum(); i++ ) {// For each cell in the row
             resultsSheet.autoSizeColumn(i);
