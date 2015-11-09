c16633b65a1695f4b3f8599916b15dfa69db0bd6
eugene.shragovich <shragovich@gmail.com>
2015-11-09 12:28:17 +0800

    extracted some static configuratuion

--- src/main/java/com/github/eugene/config/ConfigurationClass.java
+++ src/main/java/com/github/eugene/config/ConfigurationClass.java
@@ -2,6 +2,8 @@ package com.github.eugene.config;
 
 public class ConfigurationClass {
     
+    public static final String buildsDirPath = "C:\\.jenkins\\jobs\\Connect Automated Functional Tests - Firefox 32\\builds";
+    public static final String jsonRelativePath = "\\htmlreports\\Functional_Test_Report\\cucumber.json";    
     
 
 }
