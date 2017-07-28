175d9b02b7ad0a1490e74b5966f102075c48d37d
EugeneS30 <shragovich@gmail.com>
2017-07-28 11:27:50 +0800

    amended existing classes to use PropertiesContainer

--- src/main/java/com/github/eugene/processing/DataParse.java
+++ src/main/java/com/github/eugene/processing/DataParse.java
@@ -23,199 +23,202 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
-import com.github.eugene.config.ConfigurationClass;
+import com.github.eugene.config.PropertiesContainer;
 import com.github.eugene.containers.FeatureFileElement;
 import com.github.eugene.containers.Scenario;
 import com.github.eugene.containers.UniqueScenario;
 
 @Slf4j
 public class DataParse {
-    private final static String buildsDirPath = ConfigurationClass.buildsDirPath;
-    private final static String jsonRelativePath = ConfigurationClass.jsonRelativePath;
+	private final static String buildsDirPath = PropertiesContainer.getProperty("buildsDirPath");
+	private final static String jsonRelativePath = PropertiesContainer.getProperty("jsonRelativePath");
 
-    @Setter
-    @Getter
-    private static int lastBuild;
+	@Setter
+	@Getter
+	private static int lastBuild;
 
-    public static Map<Integer, List<FeatureFileElement>> extractBuildsData() throws FileNotFoundException, IOException {
+	public static Map<Integer, List<FeatureFileElement>> extractBuildsData() throws FileNotFoundException, IOException {
 
-        List<Integer> ignoredBuildsList = new ArrayList<Integer>();
-        File ignoredBuildsFile = new File(System.getProperty("user.dir") + ConfigurationClass.ignoreFileRelativePath);
+		List<Integer> ignoredBuildsList = new ArrayList<Integer>();
+		File ignoredBuildsFile = new File(System.getProperty("user.dir")
+				+ PropertiesContainer.getProperty("ignoreFileRelativePath"));
 
-        log.info("Attempting to read the ignored builds file");
-        try (BufferedReader br = new BufferedReader(new FileReader(ignoredBuildsFile))) {
-            String line;
-            while ((line = br.readLine()) != null) {
-                ignoredBuildsList.add(Integer.parseInt(line));
-            }
-        }
+		log.info("Attempting to read the ignored builds file");
+		try (BufferedReader br = new BufferedReader(new FileReader(ignoredBuildsFile))) {
+			String line;
+			while ((line = br.readLine()) != null) {
+				ignoredBuildsList.add(Integer.parseInt(line));
+			}
+		}
 
-        File dir = new File(buildsDirPath);
-        File[] buildsDir = dir.listFiles();
+		File dir = new File(buildsDirPath);
+		File[] buildsDir = dir.listFiles();
 
-        Map<Integer, List<FeatureFileElement>> buildsResults = new TreeMap<Integer, List<FeatureFileElement>>(Collections.reverseOrder());
+		Map<Integer, List<FeatureFileElement>> buildsResults = new TreeMap<Integer, List<FeatureFileElement>>(
+				Collections.reverseOrder());
 
-        JSONParser jsonParser = new JSONParser();
+		JSONParser jsonParser = new JSONParser();
 
-        for (File buildPath : buildsDir) {
+		for (File buildPath : buildsDir) {
 
-            List<FeatureFileElement> featureFileObjectsList = new ArrayList<FeatureFileElement>();
-            int buildNumber = -1; // setting dummy value
+			List<FeatureFileElement> featureFileObjectsList = new ArrayList<FeatureFileElement>();
+			int buildNumber = -1; // setting dummy value
 
-            try {
-                buildNumber = Integer.parseInt(buildPath.getName().toString());
-            } catch (NumberFormatException e) {
-                log.warn("Not a valid folder name. Skipping...: " + buildPath.getName().toString());
-                continue;
-            }
+			try {
+				buildNumber = Integer.parseInt(buildPath.getName().toString());
+			} catch (NumberFormatException e) {
+				log.warn("Not a valid folder name. Skipping...: " + buildPath.getName().toString());
+				continue;
+			}
 
-            log.info("Parsing buildData: " + buildPath);
+			log.info("Parsing buildData: " + buildPath);
 
-            log.debug("Checking if build should be ignored...");
-            if (ignoredBuildsList.contains(buildNumber)) {
-                log.info("This build is set to be IGNORED. Continuing to the next build...");
-                continue;
-            }
+			log.debug("Checking if build should be ignored...");
+			if (ignoredBuildsList.contains(buildNumber)) {
+				log.info("This build is set to be IGNORED. Continuing to the next build...");
+				continue;
+			}
 
-            try {
-                FileReader reader = new FileReader(String.join("\\", buildPath.toString(), jsonRelativePath));
+			try {
+				FileReader reader = new FileReader(String.join("\\", buildPath.toString(), jsonRelativePath));
 
-                JSONArray jsonFileData = (JSONArray) jsonParser.parse(reader);
-                List<JSONObject> allJSONFileElementsList = new ArrayList<JSONObject>();
-                allJSONFileElementsList.addAll(jsonFileData);
+				JSONArray jsonFileData = (JSONArray) jsonParser.parse(reader);
+				List<JSONObject> allJSONFileElementsList = new ArrayList<JSONObject>();
+				allJSONFileElementsList.addAll(jsonFileData);
 
-                int iter = 0;
-                for (JSONObject featureFileElement : allJSONFileElementsList) {
+				int iter = 0;
+				for (JSONObject featureFileElement : allJSONFileElementsList) {
 
-                    log.debug("Processing new featureFileElement: " + iter);
-                    iter++ ;
+					log.debug("Processing new featureFileElement: " + iter);
+					iter++;
 
-                    String name = (String) featureFileElement.get("name");
-                    String uri = (String) featureFileElement.get("uri");
-                    JSONArray elements = (JSONArray) featureFileElement.get("elements");
+					String name = (String) featureFileElement.get("name");
+					String uri = (String) featureFileElement.get("uri");
+					JSONArray elements = (JSONArray) featureFileElement.get("elements");
 
-                    featureFileObjectsList.add(new FeatureFileElement(name, uri, elements));
-                }
+					featureFileObjectsList.add(new FeatureFileElement(name, uri, elements));
+				}
 
-                buildsResults.put(buildNumber, featureFileObjectsList);
+				buildsResults.put(buildNumber, featureFileObjectsList);
 
-            }
+			}
 
-            catch (FileNotFoundException e) {
-                log.error("File not found: " + buildPath.toString());
-            } catch (IOException e) {
-                e.printStackTrace();
-            } catch (ParseException e) {
-                e.printStackTrace();
-            }
-        }
-        return buildsResults;
+			catch (FileNotFoundException e) {
+				log.error("File not found: " + buildPath.toString());
+			} catch (IOException e) {
+				e.printStackTrace();
+			} catch (ParseException e) {
+				e.printStackTrace();
+			}
+		}
+		return buildsResults;
 
-    }
+	}
 
-    private static int getLastBuildNumber(Map<Integer, List<FeatureFileElement>> buildsResults) {
+	private static int getLastBuildNumber(Map<Integer, List<FeatureFileElement>> buildsResults) {
 
-        int lastBuild = 0;
+		int lastBuild = 0;
 
-        for (Integer buildNum : buildsResults.keySet()) {
-            if (buildNum > lastBuild) {
-                lastBuild = buildNum;
-            }
-        }
+		for (Integer buildNum : buildsResults.keySet()) {
+			if (buildNum > lastBuild) {
+				lastBuild = buildNum;
+			}
+		}
 
-        return lastBuild;
+		return lastBuild;
 
-    }
+	}
 
-    public static List<UniqueScenario> extractUniqueScenarios(Map<Integer, List<FeatureFileElement>> allBuildsResults) {
+	public static List<UniqueScenario> extractUniqueScenarios(Map<Integer, List<FeatureFileElement>> allBuildsResults) {
 
-        setLastBuild(getLastBuildNumber(allBuildsResults));
+		setLastBuild(getLastBuildNumber(allBuildsResults));
 
-        final List<UniqueScenario> allBuildResults = new ArrayList<UniqueScenario>();
+		final List<UniqueScenario> allBuildResults = new ArrayList<UniqueScenario>();
 
-        for (Entry<Integer, List<FeatureFileElement>> currentBuild : allBuildsResults.entrySet()) {
-            for (FeatureFileElement featureFileElement : currentBuild.getValue()) {
-                for (Scenario scenario : featureFileElement.getScenarios()) {
-                    if (scenario.getTags().toString().contains("Manual")) {
-                        log.debug("skipping scenario tagged as @Manual");
-                        continue;
-                    }
-                    allBuildResults.add(new UniqueScenario(currentBuild.getKey(), scenario, scenario.getRunResult(), scenario.getTags()));
-                }
-            }
-        }
+		for (Entry<Integer, List<FeatureFileElement>> currentBuild : allBuildsResults.entrySet()) {
+			for (FeatureFileElement featureFileElement : currentBuild.getValue()) {
+				for (Scenario scenario : featureFileElement.getScenarios()) {
+					if (scenario.getTags().toString().contains("Manual")) {
+						log.debug("skipping scenario tagged as @Manual");
+						continue;
+					}
+					allBuildResults.add(new UniqueScenario(currentBuild.getKey(), scenario, scenario.getRunResult(),
+							scenario.getTags()));
+				}
+			}
+		}
 
-        return allBuildResults;
-    }
+		return allBuildResults;
+	}
 
-    public static SortedSet<String> createUniqueScenriosNames(List<UniqueScenario> uniqueScenariosList) {
+	public static SortedSet<String> createUniqueScenriosNames(List<UniqueScenario> uniqueScenariosList) {
 
-        log.debug("Extracting unique scenarios names");
+		log.debug("Extracting unique scenarios names");
 
-        SortedSet<String> uniqueScenarioNamesSet = new TreeSet<String>();
+		SortedSet<String> uniqueScenarioNamesSet = new TreeSet<String>();
 
-        for (UniqueScenario scenario : uniqueScenariosList) {
-            uniqueScenarioNamesSet.add(scenario.generateUriScenarioPair());
-        }
+		for (UniqueScenario scenario : uniqueScenariosList) {
+			uniqueScenarioNamesSet.add(scenario.generateUriScenarioPair());
+		}
 
-        return uniqueScenarioNamesSet;
+		return uniqueScenarioNamesSet;
 
-    }
+	}
 
-    public static List<Integer> getAllBuildsNumbers(Map<Integer, List<FeatureFileElement>> buildsData) {
+	public static List<Integer> getAllBuildsNumbers(Map<Integer, List<FeatureFileElement>> buildsData) {
 
-        log.debug("Getting builds numbers list");
+		log.debug("Getting builds numbers list");
 
-        List<Integer> buildsList = new ArrayList<Integer>();
-        for (Integer buildNum : buildsData.keySet()) {
-            buildsList.add(buildNum);
-        }
+		List<Integer> buildsList = new ArrayList<Integer>();
+		for (Integer buildNum : buildsData.keySet()) {
+			buildsList.add(buildNum);
+		}
 
-        return buildsList;
+		return buildsList;
 
-    }
+	}
 
-    private static List<Scenario> getLastBuildScenarios(List<UniqueScenario> uniqueScenariosList) {
+	private static List<Scenario> getLastBuildScenarios(List<UniqueScenario> uniqueScenariosList) {
 
-        int lastBuildNumber = getLastBuild();
-        List<Scenario> lastBuildScenariosList = new ArrayList<Scenario>();
+		int lastBuildNumber = getLastBuild();
+		List<Scenario> lastBuildScenariosList = new ArrayList<Scenario>();
 
-        for (UniqueScenario scenario : uniqueScenariosList) {
-            if (scenario.getBuildNum() == lastBuildNumber) {
-                log.info("Scenario: {} was identified in last build", scenario.getScenarioName());
-                lastBuildScenariosList.add(scenario);
-            }
-        }
+		for (UniqueScenario scenario : uniqueScenariosList) {
+			if (scenario.getBuildNum() == lastBuildNumber) {
+				log.info("Scenario: {} was identified in last build", scenario.getScenarioName());
+				lastBuildScenariosList.add(scenario);
+			}
+		}
 
-        return lastBuildScenariosList;
+		return lastBuildScenariosList;
 
-    }
+	}
 
-    public static List<UniqueScenario> removeLegacyScenarios(List<UniqueScenario> uniqueScenariosList) {
+	public static List<UniqueScenario> removeLegacyScenarios(List<UniqueScenario> uniqueScenariosList) {
 
-        // We will need to have a full list of scenarios in the last build to know which ones are
-        // relevant at the moment.
-        List<Scenario> lastBuildScenariosList = getLastBuildScenarios(uniqueScenariosList);
-        List<UniqueScenario> uniqueScenarioNamesSetClean = new ArrayList<UniqueScenario>();
+		// We will need to have a full list of scenarios in the last build to
+		// know which ones are
+		// relevant at the moment.
+		List<Scenario> lastBuildScenariosList = getLastBuildScenarios(uniqueScenariosList);
+		List<UniqueScenario> uniqueScenarioNamesSetClean = new ArrayList<UniqueScenario>();
 
-        log.debug("Looking for legacy scenarios");
+		log.debug("Looking for legacy scenarios");
 
-        // Iterate over all unique scenarios
-        for (UniqueScenario uniqueScenario : uniqueScenariosList) {
-            // for each unique scenario check if it is in the last build
-            for (Scenario scenario : lastBuildScenariosList) {
-                if (uniqueScenario.getScenarioName().equals(scenario.getScenarioName())
-                        && uniqueScenario.getUri().equals(scenario.getUri())) {
-                    uniqueScenarioNamesSetClean.add(uniqueScenario);
-                    break;
-                }
-            }
+		// Iterate over all unique scenarios
+		for (UniqueScenario uniqueScenario : uniqueScenariosList) {
+			// for each unique scenario check if it is in the last build
+			for (Scenario scenario : lastBuildScenariosList) {
+				if (uniqueScenario.getScenarioName().equals(scenario.getScenarioName())
+						&& uniqueScenario.getUri().equals(scenario.getUri())) {
+					uniqueScenarioNamesSetClean.add(uniqueScenario);
+					break;
+				}
+			}
 
-        }
+		}
 
-        // return uniqueScenariosList;
-        return uniqueScenarioNamesSetClean;
+		return uniqueScenarioNamesSetClean;
 
-    }
+	}
 
 }
--- src/test/java/com/github/eugene/parse/ParseResultsTest.java
+++ src/test/java/com/github/eugene/parse/ParseResultsTest.java
@@ -1,13 +1,12 @@
 package com.github.eugene.parse;
 
-import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 import java.util.SortedSet;
 
 import org.junit.Test;
 
-import com.github.eugene.config.ConfigurationClass;
+import com.github.eugene.config.PropertiesContainer;
 import com.github.eugene.containers.FeatureFileElement;
 import com.github.eugene.containers.UniqueScenario;
 import com.github.eugene.processing.DataParse;
@@ -15,39 +14,45 @@ import com.github.eugene.processing.ExcelProcessor;
 
 public class ParseResultsTest {
 
-    @Test
-    public void testFunction() throws IOException {
+	@Test
+	public void testFunction() throws Exception {
 
-        // Parsing the data from all builds to a map of form (buildNumber, FeatureFileElement)
-        Map<Integer, List<FeatureFileElement>> buildsData = DataParse.extractBuildsData();
+		// Parsing the data from all builds to a map of form (buildNumber,
+		// FeatureFileElement)
+		Map<Integer, List<FeatureFileElement>> buildsData = DataParse.extractBuildsData();
 
-        // A list of UniqueScenario objects (buildNum, scenario, isFailed)
-        List<UniqueScenario> uniqueScenariosList = DataParse.extractUniqueScenarios(buildsData);
-        List<UniqueScenario> uniqueScenariosListCleaned = DataParse.removeLegacyScenarios(uniqueScenariosList);
+		// A list of UniqueScenario objects (buildNum, scenario, isFailed)
+		List<UniqueScenario> uniqueScenariosList = DataParse.extractUniqueScenarios(buildsData);
+		List<UniqueScenario> uniqueScenariosListCleaned = DataParse.removeLegacyScenarios(uniqueScenariosList);
 
-        if (ConfigurationClass.removeLegacyScenarios) {
+		if (PropertiesContainer.getProperty("removeLegacyScenarios").equals("true")) {
 
-            // Generates a list of unique scenario names from all the builds.
-            SortedSet<String> uniqueScenarioNamesSet = DataParse.createUniqueScenriosNames(uniqueScenariosListCleaned);
+			// Generates a list of unique scenario names from all the builds.
+			SortedSet<String> uniqueScenarioNamesSet = DataParse.createUniqueScenriosNames(uniqueScenariosListCleaned);
 
-            // A list of all builds numbers
-            List<Integer> allBuildsNumbersList = DataParse.getAllBuildsNumbers(buildsData);
+			// A list of all builds numbers
+			List<Integer> allBuildsNumbersList = DataParse.getAllBuildsNumbers(buildsData);
 
-            ExcelProcessor.prepareProcessor(allBuildsNumbersList, uniqueScenarioNamesSet);
-            ExcelProcessor.processData(uniqueScenariosListCleaned);
+			ExcelProcessor.prepareProcessor(allBuildsNumbersList, uniqueScenarioNamesSet);
+			ExcelProcessor.processData(uniqueScenariosListCleaned);
 
-        } else {
+		} else if (PropertiesContainer.getProperty("removeLegacyScenarios").equals("false")) {
 
-            SortedSet<String> uniqueScenarioNamesSet = DataParse.createUniqueScenriosNames(uniqueScenariosList);
+			SortedSet<String> uniqueScenarioNamesSet = DataParse.createUniqueScenriosNames(uniqueScenariosList);
 
-            // A list of all builds numbers
-            List<Integer> allBuildsNumbersList = DataParse.getAllBuildsNumbers(buildsData);
+			// A list of all builds numbers
+			List<Integer> allBuildsNumbersList = DataParse.getAllBuildsNumbers(buildsData);
 
-            ExcelProcessor.prepareProcessor(allBuildsNumbersList, uniqueScenarioNamesSet);
-            ExcelProcessor.processData(uniqueScenariosList);
-        }
+			ExcelProcessor.prepareProcessor(allBuildsNumbersList, uniqueScenarioNamesSet);
+			ExcelProcessor.processData(uniqueScenariosList);
 
-        ExcelProcessor.finaliseProcessor();
+		} else {
 
-    }
+			throw new Exception("removeLegacyScenarios property must be set to false or true");
+
+		}
+
+		ExcelProcessor.finaliseProcessor();
+
+	}
 }
