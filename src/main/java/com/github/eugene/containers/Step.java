a4351c01280e98106fef4d8e9d5465fb32cb3a23
EugeneS30 <shragovich@gmail.com>
2017-07-31 11:38:49 +0800

    added lombok logging, removed redundant logging

--- src/main/java/com/github/eugene/containers/Step.java
+++ src/main/java/com/github/eugene/containers/Step.java
@@ -1,10 +1,12 @@
 package com.github.eugene.containers;
 
+import lombok.extern.slf4j.Slf4j;
+
 import org.apache.log4j.Logger;
 import org.json.simple.JSONObject;
 
+@Slf4j
 public class Step {
-    final static Logger log = Logger.getLogger(Step.class);
 
 	private String name;
 	private String result;
@@ -12,20 +14,17 @@ public class Step {
 	private String output;
 
 	public Step(String name, JSONObject result, String output) {
-	    log.debug("Step constructor start...");
-	    
+
 		this.name = name;
 		this.result = (String) result.get("status");
 		if ("skipped".equals(this.result)) {
-		    log.debug("SKIPPED test. Setting duration to -1");
-		    this.duration = 0;
-		}
-		else if ("undefined".equals(this.result)) {
-		    log.debug("UNDEFINED test.  Setting duration to -2");
-		    this.duration = 0;
-		}
-		else {
-		    this.duration = (Long) result.get("duration");
+			log.debug("SKIPPED test. Setting duration to -1");
+			this.duration = 0;
+		} else if ("undefined".equals(this.result)) {
+			log.debug("UNDEFINED test.  Setting duration to -2");
+			this.duration = 0;
+		} else {
+			this.duration = (Long) result.get("duration");
 		}
 		this.output = output;
 
@@ -38,33 +37,32 @@ public class Step {
 
 		return false;
 	}
-	
+
 	public boolean isUndefined() {
-        if ("undefined".equalsIgnoreCase(result)) {
-            return true;
-        }
+		if ("undefined".equalsIgnoreCase(result)) {
+			return true;
+		}
 
-        return false;
-    }
-	
+		return false;
+	}
+
 	public boolean isSkipped() {
-        if ("skipped".equalsIgnoreCase(result)) {
-            return true;
-        }
+		if ("skipped".equalsIgnoreCase(result)) {
+			return true;
+		}
 
-        return false;
-    }
-	
+		return false;
+	}
+
 	public String getName() {
 		return name;
 	}
-	
-	public String getOutput() {
-	    if (!"empty".equals(output)) {
-	        return output;
-	    }
-	    
-	    return "empty";
-	}
 
+	public String getOutput() {
+		if (!"empty".equals(output)) {
+			return output;
+		}
+
+		return "empty";
+	}
 }
